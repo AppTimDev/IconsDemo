b83cc5c9cad0d7cff20335551ab7ef4802f5f85b
AppTimDev <apptimdev@gmail.com>
2023-06-06 19:15:31 +0800

    navbar

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,14 +1,32 @@
+function toggle(el) {
+    if (el.style.display == 'none') {
+        el.style.display = 'unset';
+    } else {
+        el.style.display = 'none';
+    }
+}
 ready(function () {
-    var icons = document.getElementsByClassName("vicon_container");
+    const icons = document.getElementsByClassName("vicon_container");
     for (let i = 0; i < icons.length; i++) {
         const icon = icons[i];
-        icon.addEventListener("mouseenter", (e)=>{
-            var add = e.target.getElementsByClassName("vicon_add")[0];
-            add.style="display:unset;"
+        icon.addEventListener("mouseenter", (e) => {
+            const add = e.target.getElementsByClassName("vicon_add")[0];
+            add.style = "display:unset;"
         }, false);
-        icon.addEventListener("mouseleave", (e)=>{
-            var add = e.target.getElementsByClassName("vicon_add")[0];
-            add.style="display:none;"             
+        icon.addEventListener("mouseleave", (e) => {
+            const add = e.target.getElementsByClassName("vicon_add")[0];
+            add.style = "display:none;"
         }, false);
-    }    
+    }
+
+    const menuBtns = document.getElementsByClassName("menu-btn");
+    for (let i = 0; i < menuBtns.length; i++) {
+        const menuBtn = menuBtns[i];
+        menuBtn.addEventListener("click", (e) => {
+            const menuIcon = document.getElementById('menu-btn-icon-menu')
+            const cancelIcon = document.getElementById('menu-btn-icon-cancel')
+            toggle(menuIcon)
+            toggle(cancelIcon)            
+        }, false);
+    }
 });
