147fb96715b7e0398ba7960ba3903da18c7ffb73
AppTimDev <apptimdev@gmail.com>
2023-06-07 00:08:21 +0800

    scroll to top and others

--- src/js/site/order/root.js
+++ src/js/site/order/root.js
@@ -1,7 +1,27 @@
 function ready(fn) {
     if (document.readyState !== 'loading') {
-      fn();
+        fn();
     } else {
-      document.addEventListener('DOMContentLoaded', fn);
+        document.addEventListener('DOMContentLoaded', fn);
     }
 }
+
+// scroll to top
+// when user clicks on the scroll-to-top button, scroll to the top of the document
+function ScrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+function scroll() {
+    // when user scrolls down 20px from the top of the document
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+ready(function(){
+    let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    scrollToTopBtn.addEventListener('click', ScrollToTop);
+    document.addEventListener('scroll', scroll);
+})
